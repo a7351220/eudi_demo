c3c2f0a17f24fa5c7641ae12775ca90533231f02
minghua <a7351220@gmail.com>
2024-03-20 16:07:53 +0800

    修改生產url-3

--- next.config.js
+++ next.config.js
@@ -1,16 +1,34 @@
-/** @type {import('next').NextConfig} */
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   rewrites: async () => {
+//     return [
+//       {
+//         source: '/api/:path*',
+//         destination:
+//           process.env.NODE_ENV === 'development'
+//             ? 'http://127.0.0.1:5328/api/:path*'
+//             : '/api',
+//       },
+//     ]
+//   },
+// }
+
+// module.exports = nextConfig
+
 const nextConfig = {
   rewrites: async () => {
-    return [
-      {
-        source: '/api/:path*',
-        destination:
-          process.env.NODE_ENV === 'development'
-            ? 'http://127.0.0.1:5328/api/:path*'
-            : 'https://eudi-demo.vercel.app/api/:path*',
-      },
-    ]
+    if (process.env.NODE_ENV === 'development') {
+      // 仅在开发环境中使用重写规则
+      return [
+        {
+          source: '/api/:path*',
+          destination: 'http://127.0.0.1:5328/api/:path*',
+        },
+      ];
+    }
+    // 生产环境不需要重写规则
+    return [];
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
